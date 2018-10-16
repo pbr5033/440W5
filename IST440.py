1a6e46b912c05a7fcd84e8eb83caa60d256e1c0a
Paul Rex Rivera <paulrexrivera@Pauls-MacBook-Pro.local>
2018-10-16 15:54:02 -0400

    IST440 py
    Co-Authored-By: wilsonchenn <wilsonchenn@users.noreply.github.com>

---
+++ IST440.py
@@ -0,0 +1,14 @@
+import cherrypy;
+import string;
+import random;
+
+#studentinputs
+student_name = raw_input("What is your first name");
+psu_email = raw_input("What is your Penn State Email");
+date = raw_input("What is the date(example 01/10/98) today?");sadasasdasdasd
+
+
+#Studentvariables stored
+inputstudent_name = student_name;
+inputpsu_email = psu_email;
+inputdate = date;
