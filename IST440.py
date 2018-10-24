42d65f4424835ca3eed1fcf75c5a31f73d54b6f1
Paul Rex Rivera <paulrexrivera@Pauls-MBP.hsd1.pa.comcast.net>
2018-10-24 10:49:00 -0400

    Test
    Nawaf

--- IST440.py
+++ IST440.py
@@ -1,11 +1,93 @@
-import cherrypy;
-import string;
-import random;
+import string
+import base64
+import binascii
+import random
+import hashlib
+import cherrypy
+import subprocess
+from time import gmtime, strftime
 
+
+#Make the CherryPy server answer on any IP address that is received by this server. This makes the CherryPy server run from inside of a NAT boundary where the inside and outside IP addresses may be different.
+cherrypy.server.socket_host = '0.0.0.0'
+
+class PolyEncrypt(object):
+
+	global rot47
+
+	def rot47(s):
+    		x = []
+	    	for i in xrange(len(s)):
+        		j = ord(s[i])
+        		if j >= 33 and j <= 126:
+            			x.append(chr(33 + ((j + 14) % 94)))
+        		else:
+            			x.append(s[i])
+    		return ''.join(x)
+
+	global rot13
+
+	def rot13(s):
+    		rotate = string.maketrans("ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+    		return ''.join(string.translate(s, rotate))
+
+#============================================================
 #studentinputs
-student_name = raw_input("What is your first name");
-psu_email = raw_input("What is your Penn State Email");
-date = raw_input("What is the date(example 01/10/98) today?");sadasasdasdasd
+#============================================================
+#Student Section
+tudent_name = raw_input("What is your first name")
+
+#Email Section
+@cherrypy.expose
+        def index(self):
+        # This is the page that the user hits when they first find the system.  They need to provide an email
+        # address.
+        # This web form collects the email address and sends it off to be confirmed.
+
+                return """<html>
+                        <head>
+                                <title>PolyEncrypt</title>
+                                <link rel="stylesheet" href="polylab/style.css" type="text/css" />
+                        </head>
+                <body>
+                <div id="content">
+                        <h1>PolyEncrypt</h1>
+                        <h2>Delivery System</h2>
+                        <br>
+                        <br>
+                        <h3>Please enter your email address</h3>
+                        <p><i>(e.g. abc1234@psu.edu)</i></p>
+                        <form method="get" action="encryptchallenges">
+                                <input type="text" value="" name="email" />
+                                <button type="submit">Submit</button>
+                        </form>
+                </body>
+		</html>"""
+
+
+
+
+email = raw_input("What is your Penn State Email")
+class Polynet(object):
+    global checkmail
+
+    def checkmail(email):
+        #this function validates the email address's format
+        #we expect the emailaddress to be in the form aaa###@psu.edu
+
+        email= email
+        from validate_email import validate_email
+        is_valid = validate_email(email)
+        return is_valid
+
+
+
+
+
+
+
+
+date = raw_input("What is the date(example 01/10/98) today?")
 
 
 #Studentvariables stored
